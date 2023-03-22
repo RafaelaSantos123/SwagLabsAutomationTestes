e4adc4e058c0ee71cbf845f00775ba1cc0f2acbf
RafaelaSantos123 <128647512+RafaelaSantos123@users.noreply.github.com>
2023-03-22 19:17:48 -0300

    Add files via upload

---
+++ SwagLabsAutomationTestes/src/main/java/com/example/SwagLabsAutomationTestes/SwagLabsAutomationTestesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SwagLabsAutomationTestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwagLabsAutomationTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwagLabsAutomationTestesApplication.class, args);
+	}
+
+}
---
+++ SwagLabsAutomationTestes/src/test/java/com/example/SwagLabsAutomationTestes/SwagLabsAutomationTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SwagLabsAutomationTestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwagLabsAutomationTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
