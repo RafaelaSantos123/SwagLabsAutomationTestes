e0ec54a2aea2a6b8a4575aa5c456836e660cb8c3
“RafaelaSantos123” <rafaela_mam@hotmail.com>
2023-03-29 20:10:42 -0300

    Criacao de teste para validar o projeto

--- SwagLabsAutomationTestes/src/test/java/com/example/SwagLabsAutomationTestes/SwagLabsAutomationTestesApplicationTests.java
+++ SwagLabsAutomationTestes/src/test/java/com/example/SwagLabsAutomationTestes/SwagLabsAutomationTestesApplicationTests.java
@@ -1,15 +1,31 @@
 package com.example.SwagLabsAutomationTestes;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SwagLabsAutomationTestesApplicationTests {
-
+	WebDriver driver;
 	@Test
 	void contextLoads() {
 		System.out.println("Isadora");
 		System.out.println("Rafaela");
+
+	}
+	@Test
+	void acessarSite(){
+
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(ops);
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+
 	}
 
 }
