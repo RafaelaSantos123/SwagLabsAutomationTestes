8cb5f4f1a181007081c20429517c581def64d9a6
“isadoraalves” <isadoraoliveira1997@icloud.com>
2023-05-03 22:14:31 -0300

    criaçao do scroll no core e alteraçao no teste de compra

--- SwagLabsAutomationTestes/src/test/java/com/example/SwagLabsAutomationTestes/core/core.java
+++ SwagLabsAutomationTestes/src/test/java/com/example/SwagLabsAutomationTestes/core/core.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -62,5 +63,11 @@ public class core {
         element.sendKeys(text);
    }
 
+   public static void scroll (By locator){
+       WebElement iframe = driver.findElement(locator);
+       new Actions(driver)
+               .scrollToElement(iframe)
+               .perform();
+   }
 }
 
--- SwagLabsAutomationTestes/src/test/java/com/example/SwagLabsAutomationTestes/pages/compraPage.java
+++ SwagLabsAutomationTestes/src/test/java/com/example/SwagLabsAutomationTestes/pages/compraPage.java
@@ -12,12 +12,26 @@ public class compraPage extends core {
 
     public By produtoTres = By.id("add-to-cart-test.allthethings()-t-shirt-(red)");
 
+    public By produtoQuatro = By.id("add-to-cart-sauce-labs-bike-light");
+
+    public By produtoCinco = By.id("add-to-cart-sauce-labs-bolt-t-shirt");
+
+    public By produtoSeis = By.id("add-to-cart-sauce-labs-onesie");
+
+    public By btnCheckout  = By.id("checkout");
+
+    public By btnCarrinho  = By.id("shopping_cart_container");
 
     public void realizarCompra() throws InterruptedException {
 
         clicar(produtoUm);
         clicar(produtoDois);
         clicar(produtoTres);
+        clicar(produtoQuatro);
+        clicar(produtoCinco);
+        clicar(produtoSeis);
+        clicar(btnCarrinho);
+        scroll(btnCheckout);
 
     }
 
